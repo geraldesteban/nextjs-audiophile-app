5c6ac762c40900e233849242a98bb6c92d266074
geraldesteban <wakin.esteban@gmail.com>
2025-09-20 17:25:54 +0800

    refactor: add reusable content in the footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,41 +1,76 @@
 import Logo from "@/app/_components/Logo";
-import Navigation from "./_components/Navigation";
 import Link from "next/link";
 import Image from "next/image";
 import facebook from "@/app/_assets/SocialMedia/icon-facebook.svg";
 import instagram from "@/app/_assets/SocialMedia/icon-instagram.svg";
 import twitter from "@/app/_assets/SocialMedia/icon-twitter.svg";
+import SectionFour from "./_components/Home/SectionFour";
 
 function Footer() {
   return (
-    <div className="bg-[#101010] flex justify-between p-32">
-      <div>
-        <Logo />
-        <p className="text-gray-500 text-[15px] w-[530px] mt-10 mb-20">
+    <>
+      <SectionFour />
+      <div className="bg-[#101010] pt-20 pb-10 px-32 max-lg:p-10">
+        <div className="flex justify-between items-center mb-10 max-lg:block">
+          <div className="max-sm:flex max-sm:place-content-center">
+            <Logo />
+          </div>
+          <nav className="max-lg:mt-10">
+            <ul className="text-white text-[13px] font-bold tracking-widest flex items-center gap-10 max-xl:-tracking-wider max-xl:gap-5 max-sm:block max-sm:text-center">
+              <li className="mb-5 hover:text-[#D87D4A]">
+                <Link href="/">HOME</Link>
+              </li>
+              <li className="mb-5 hover:text-[#D87D4A]">
+                <Link href="/">HEADPHONES</Link>
+              </li>
+              <li className="mb-5 hover:text-[#D87D4A]">
+                <Link href="/">SPEAKERS</Link>
+              </li>
+              <li className="mb-5 hover:text-[#D87D4A]">
+                <Link href="/">EARPHONES</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className="text-gray-500 text-[15px] w-[530px] mb-20 max-lg:w-[680px] max-md:w-[530px] max-sm:text-center max-sm:w-[100%]">
           Audiophile is an all in one stop to fulfill your audio needs.
           We&apos;re a small team of music lovers and sound specialists who are
           devoted to helping you get the most out of personal audio. Come and
           visit our demo facility - we&apos;re open 7 days a week.
         </p>
-        <span className="text-gray-500 text-[15px]">
-          Copyright 2025. All Rights Reserved
-        </span>
-      </div>
-      <div>
-        <Navigation />
-        <div className="flex justify-end items-center gap-10 mt-32">
-          <Link href="/">
-            <Image src={facebook} quality={100} alt="Facebook" />
-          </Link>
-          <Link href="/">
-            <Image src={instagram} quality={100} alt="Instagram" />
-          </Link>
-          <Link href="/">
-            <Image src={twitter} quality={100} alt="Twitter" />
-          </Link>
+        <div className="flex items-center justify-between max-sm:block max-sm:justify-center">
+          <p className="text-gray-500 text-[15px] font-bold text-center">
+            Copyright 2025. All Rights Reserved
+          </p>
+          <div className="flex justify-end items-center gap-10 max-lg:gap-5 max-sm:justify-center max-sm:mt-10">
+            <Link href="/">
+              <Image
+                src={facebook}
+                quality={100}
+                alt="Facebook"
+                className="text-[#D87D4A]"
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                src={instagram}
+                quality={100}
+                alt="Instagram"
+                className="text-[#D87D4A]"
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                src={twitter}
+                quality={100}
+                alt="Twitter"
+                className="text-[#D87D4A]"
+              />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,62 @@
 import Image from "next/image";
-import hero from "@/public/image-hero.jpg";
+import heroDesktop from "@/public/image-hero.jpg";
+import heroTablet from "@/public/image-hero-tablet.jpg";
+import heroMobile from "@/public/image-hero-mobile.jpg";
 import Link from "next/link";
 import SectionTwo from "./_components/Home/SectionTwo";
 import SectionThree from "./_components/Home/SectionThree";
-import SectionFour from "./_components/Home/SectionFour";
+import { getAllHeadphones } from "./_lib/actions/getAllHeadphones";
 
 /* Home */
-function Page() {
+async function Page() {
+  const allHeadphones = await getAllHeadphones();
+  console.log(allHeadphones);
+
   return (
     <div>
       <main>
-        <div className="z-10 relative ml-32 mt-52">
-          <h2 className="text-[#FAFAFA] text-sm tracking-[8px] mb-5">
+        <div className="z-10 relative ml-32 mt-52 max-lg:text-center max-lg:mx-auto max-sm:mt-20">
+          <h2 className="text-[#FAFAFA] text-[14px] tracking-[8px] mb-5">
             NEW PRODUCT
           </h2>
-          <h2 className="text-white text-[56px] font-bold mb-10">
+          <h2 className="text-white text-[56px] font-bold mb-10 max-sm:text-[36px]">
             XX99 MARK II
             <br />
             HEADPHONES
           </h2>
-          <p className="text-[#F1F1F1] w-[360px] mb-10 ">
+          <p className="text-[#F1F1F1] text-[15px] w-[360px] mb-10 max-lg:mx-auto max-sm:w-[300px]">
             Experience natural, life like audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
           <Link
             href="/"
-            className="text-white font-bold bg-[#D87D4A] px-7 py-3"
+            className="text-white font-bold bg-[#D87D4A] px-7 py-3 hover:brightness-130"
           >
             SEE PRODUCT
           </Link>
         </div>
         <Image
-          src={hero}
+          src={heroDesktop}
           fill
           quality={100}
           placeholder="blur"
-          className="object-cover object-top rounded-bl-xl rounded-br-xl"
+          className="object-cover object-top rounded-bl-xl max-lg:hidden"
+          alt="Hero background"
+        />
+        <Image
+          src={heroTablet}
+          fill
+          quality={100}
+          placeholder="blur"
+          className="object-cover object-top hidden max-lg:block"
+          alt="Hero background"
+        />
+        <Image
+          src={heroMobile}
+          fill
+          quality={100}
+          placeholder="blur"
+          className="object-cover object-bottom hidden max-sm:block"
           alt="Hero background"
         />
       </main>
@@ -43,8 +64,6 @@ function Page() {
       <SectionTwo />
       {/* Section Three */}
       <SectionThree />
-      {/* Section Four */}
-      <SectionFour />
     </div>
   );
 }
