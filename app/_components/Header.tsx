a7988dd95aade972f1aa8b8df3c8e7d59378db79
geraldesteban <wakin.esteban@gmail.com>
2025-09-20 17:23:21 +0800

    feat: add menu icon in table size below

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,22 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import CartIcon from "@/app/_components/Icons/CartIcon";
+import menu from "@/app/_assets/Icons/icon-hamburger.svg";
+import Image from "next/image";
 
 function Header() {
   return (
     <header className="border-b border-[#979797] bg-black">
-      <div className="flex justify-between items-center px-32 py-10">
-        <Logo />
+      <div className="flex justify-between items-center px-32 py-10 max-lg:px-10">
+        <div className="flex items-center">
+          <Image
+            src={menu}
+            quality={100}
+            alt="Menu"
+            className="z-10 hidden mr-10 max-lg:block"
+          />
+          <Logo />
+        </div>
         <Navigation />
         <CartIcon />
       </div>
