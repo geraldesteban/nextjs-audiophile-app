0cc59914dd6da2b07986a532751682a7ae103499
geraldesteban <wakin.esteban@gmail.com>
2025-09-20 17:29:53 +0800

    feat: add pages for earphones, headphones, and speakers

---
+++ app/earphones/page.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Earphones",
+};
+
+function Page() {
+  return <h2>TEST</h2>;
+}
+
+export default Page;
---
+++ app/speakers/page.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Speakers",
+};
+
+function Page() {
+  return <h2>TEST</h2>;
+}
+
+export default Page;
