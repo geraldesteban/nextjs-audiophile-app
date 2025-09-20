1ef8f744abaacc45d3055cf7d2b3d6bd8a2669a3
geraldesteban <wakin.esteban@gmail.com>
2025-09-20 17:24:01 +0800

    feat: add href of navigations

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 function Navigation() {
   return (
-    <nav className="z-10">
-      <ul className="text-white text-[13px] font-bold tracking-widest flex items-center gap-10">
-        <li>
+    <nav className="z-10 max-lg:hidden">
+      <ul className="text-white text-[13px] font-bold tracking-widest flex items-center gap-10 max-xl:-tracking-wider max-xl:gap-5">
+        <li className="hover:text-[#D87D4A]">
           <Link href="/">HOME</Link>
         </li>
-        <li>
-          <Link href="/">HEADPHONES</Link>
+        <li className="hover:text-[#D87D4A]">
+          <Link href="/headphones">HEADPHONES</Link>
         </li>
-        <li>
-          <Link href="/">SPEAKERS</Link>
+        <li className="hover:text-[#D87D4A]">
+          <Link href="/speakers">SPEAKERS</Link>
         </li>
-        <li>
-          <Link href="/">EARPHONES</Link>
+        <li className="hover:text-[#D87D4A]">
+          <Link href="/earphones">EARPHONES</Link>
         </li>
       </ul>
     </nav>
