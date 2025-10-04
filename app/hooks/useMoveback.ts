a36271f9f60b5fb7d5ae39db243daf7d957ec91f
geraldesteban <wakin.esteban@gmail.com>
2025-10-04 20:29:54 +0800

    chore: add hooks folder then add usemoveback.ts

---
+++ app/hooks/useMoveback.ts
@@ -0,0 +1,13 @@
+import { useRouter } from "next/navigation";
+
+function useMoveBack(): () => void {
+  const router = useRouter();
+
+  const moveBack = (): void => {
+    router.back();
+  };
+
+  return moveBack;
+}
+
+export default useMoveBack;
