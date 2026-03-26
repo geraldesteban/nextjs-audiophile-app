3f031c9d99a0a5776d1a74d47cd937ebf3605702
geraldesteban <wakin.esteban@gmail.com>
2026-03-27 00:40:36 +0800

    feat: add types for earphones, headphones, and speakers

---
+++ app/types/earphones.ts
@@ -0,0 +1,38 @@
+export type Earphones = {
+  id: number;
+  slug: string;
+  name: string;
+
+  image: Record<string, unknown>;
+  category: string;
+
+  categoryImage?: Record<string, unknown>;
+
+  new: boolean;
+  price: number;
+
+  description?: string;
+  features?: string;
+
+  includes: {
+    quantity: number;
+    item: string;
+  }[];
+
+  gallery: {
+    first: {
+      desktop: string;
+      tablet: string;
+      mobile: string;
+    };
+  };
+
+  others: unknown[];
+
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type EarphonesProps = {
+  earphones: Earphones[];
+};
