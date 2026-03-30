b09787c04fdd33f9d7d383b4553146243b2f456b
geraldesteban <wakin.esteban@gmail.com>
2026-03-30 20:41:50 +0800

    test

--- app/earphones/page.tsx
+++ app/earphones/page.tsx
@@ -1,28 +1,28 @@
 // import { getProducts } from "@/app/_lib/services/getProducts";
 
-// import HomeNavigation from "@/app/_components/Home/HomeNavigation";
-// import HomeAudioGear from "@/app/_components/Home/HomeAudioGear";
-// import Products from "@/app/_components/Products/Products";
+import HomeNavigation from "@/app/_components/Home/HomeNavigation";
+import HomeAudioGear from "@/app/_components/Home/HomeAudioGear";
+import Products from "@/app/_components/Products/Products";
 
-// export const metadata = {
-//   title: "Earphones",
-// };
+export const metadata = {
+  title: "Earphones",
+};
 
-// async function Page() {
-//   const products = await getProducts({ productName: "earphones" });
+async function Page() {
+  // const products = await getProducts({ productName: "earphones" });
 
-//   return (
-//     <div>
-//       <div className="bg-black pt-52 pb-32 max-xl:px-16 max-lg:py-20 max-sm:py-10">
-//         <h2 className="text-white text-[40px] font-bold text-center max-sm:text-[28px]">
-//           EARPHONES
-//         </h2>
-//       </div>
-//       <Products products={products} />
-//       <HomeNavigation />
-//       <HomeAudioGear />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <div className="bg-black pt-52 pb-32 max-xl:px-16 max-lg:py-20 max-sm:py-10">
+        <h2 className="text-white text-[40px] font-bold text-center max-sm:text-[28px]">
+          EARPHONES
+        </h2>
+      </div>
+      {/* <Products products={products} /> */}
+      <HomeNavigation />
+      <HomeAudioGear />
+    </div>
+  );
+}
 
-// export default Page;
+export default Page;
--- app/speakers/[productName]/page.tsx
+++ app/speakers/[productName]/page.tsx
@@ -1,40 +1,40 @@
 // import { getProductDetails } from "@/app/_lib/services/getProductDetails";
 
-// import Gallery from "@/app/_components/Gallery";
-// import YouMayAlsoLike from "@/app/_components/YouMayAlsoLike";
-// import HomeNavigation from "@/app/_components/Home/HomeNavigation";
-// import HomeAudioGear from "@/app/_components/Home/HomeAudioGear";
+import Gallery from "@/app/_components/Gallery";
+import YouMayAlsoLike from "@/app/_components/YouMayAlsoLike";
+import HomeNavigation from "@/app/_components/Home/HomeNavigation";
+import HomeAudioGear from "@/app/_components/Home/HomeAudioGear";
 // import ProductDetails from "@/app/_components/Products/productDetails";
 
-// export async function generateMetadata({
-//   params,
-// }: {
-//   params: { productName: string };
-// }) {
-//   const { productName } = await params;
+export async function generateMetadata({
+  params,
+}: {
+  params: { productName: string };
+}) {
+  const { productName } = await params;
 
-//   return {
-//     title: `${productName}`,
-//   };
-// }
+  return {
+    title: `${productName}`,
+  };
+}
 
-// async function Page({ params }: { params: { productName: string } }) {
-//   const { productName } = await params;
+async function Page({ params }: { params: { productName: string } }) {
+  //   const { productName } = await params;
 
-//   const productDetails = await getProductDetails({
-//     productName: "speakers",
-//     productNameDetails: `${productName}`,
-//   });
+  //   const productDetails = await getProductDetails({
+  //     productName: "speakers",
+  //     productNameDetails: `${productName}`,
+  //   });
 
-//   return (
-//     <div>
-//       <ProductDetails products={productDetails} />
-//       <Gallery />
-//       <YouMayAlsoLike />
-//       <HomeNavigation />
-//       <HomeAudioGear />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {/* <ProductDetails products={productDetails} /> */}
+      <Gallery />
+      <YouMayAlsoLike />
+      <HomeNavigation />
+      <HomeAudioGear />
+    </div>
+  );
+}
 
-// export default Page;
+export default Page;
