65f776f1414d92a7d767783a5b782a9f84e0f149
geraldesteban <wakin.esteban@gmail.com>
2026-04-01 22:27:09 +0800

    test

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/app/_components/Spinners/Spinner";
+
+function Loading() {
+  return (
+    <div className="min-h-screen">
+      <Spinner />
+    </div>
+  );
+}
+
+export default Loading;
