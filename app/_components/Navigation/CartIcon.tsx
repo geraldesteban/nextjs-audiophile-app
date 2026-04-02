fee50515e26759bec7c9a3f27fd8dff1890afc50
geraldesteban <wakin.esteban@gmail.com>
2026-04-02 20:24:58 +0800

    Add carts count if adding to the cart

--- app/_components/Navigation/CartIcon.tsx
+++ app/_components/Navigation/CartIcon.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import cart from "@/public/icon-cart.svg";
+import { useCartStore } from "@/app/store/cartStore";
 
 function CartIcon() {
+  const carts = useCartStore((state) => state.cart);
+
   return (
     <div className="relative flex items-center mr-5">
       <button className="cursor-pointer">
         <Image src={cart} quality={100} alt="cart icon" />
+        <span className="absolute bottom-1 left-3 bg-[#D87D4A]/50 w-8 h-8 rounded-full flex items-center justify-center text-white">
+          {carts.length}
+        </span>
       </button>
     </div>
   );
