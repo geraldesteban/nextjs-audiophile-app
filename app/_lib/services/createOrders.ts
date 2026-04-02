c758df66af66fd1070418b0b1076fa9ac232826a
geraldesteban <wakin.esteban@gmail.com>
2026-04-02 20:18:21 +0800

    Add service for saving orders to the database

---
+++ app/_lib/services/createOrders.ts
@@ -0,0 +1,20 @@
+import { connectDB } from "../config/mongodb";
+import Orders from "../models/orders";
+
+export async function createOrders(
+  carts: CartItemDetails[],
+  customer: CustomerDetails,
+) {
+  await connectDB();
+
+  const order = {
+    orderId: crypto.randomUUID(),
+    items: carts,
+    customer,
+    status: "pending",
+  };
+
+  const storeOrder = await Orders.create(order);
+
+  return storeOrder;
+}
