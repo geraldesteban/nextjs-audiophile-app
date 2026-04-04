50d357e8103970254800db07bccfa766f2006e23
geraldesteban <wakin.esteban@gmail.com>
2026-04-05 01:56:34 +0800

    Fix for swapping products based on the subroute

--- app/_components/YouMayAlsoLike.tsx
+++ app/_components/YouMayAlsoLike.tsx
@@ -1,138 +1,132 @@
 import Image from "next/image";
 import Link from "next/link";
+import Transition from "@/app/_components/Transition";
+
 /* desktop */
 import XXMARKI from "@/app/_assets/YouMayAlsoLike/desktop/image-xx99-mark-one-headphones.jpg";
 import XX59 from "@/app/_assets/YouMayAlsoLike/desktop/image-xx59-headphones.jpg";
 import ZX9 from "@/app/_assets/YouMayAlsoLike/desktop/image-zx9-speaker.jpg";
+
 /* tablet */
 import XXMARKITablet from "@/app/_assets/YouMayAlsoLike/tablet/image-xx99-mark-one-headphones.jpg";
 import XX59Tablet from "@/app/_assets/YouMayAlsoLike/tablet/image-xx59-headphones.jpg";
 import ZX9Tablet from "@/app/_assets/YouMayAlsoLike/tablet/image-zx9-speaker.jpg";
+
 /* mobile */
 import XXMARKIMobile from "@/app/_assets/YouMayAlsoLike/mobile/image-xx99-mark-one-headphones.jpg";
 import XX59Mobile from "@/app/_assets/YouMayAlsoLike/mobile/image-xx59-headphones.jpg";
 import ZX9Mobile from "@/app/_assets/YouMayAlsoLike/mobile/image-zx9-speaker.jpg";
 
-function YouMayAlsoLike() {
+import { getHeadphones } from "@/app/_lib/services/headphones/getHeadphones";
+import { getSpeakers } from "@/app/_lib/services/speakers/getSpeakers";
+import { getEarphones } from "../_lib/services/earphones/getEarphones";
+
+export default async function YouMayAlsoLike({
+  productName,
+}: {
+  productName: string;
+}) {
+  const headphones = await getHeadphones();
+  const speakers = await getSpeakers();
+  const earphones = await getEarphones();
+
+  const base = [...headphones, ...speakers, ...earphones];
+
+  let youMayAlsoLike = [];
+
+  console.log("productName:", JSON.stringify(productName));
+
+  if (productName === "xx99-mark-two-headphones") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "xx99-mark-one-headphones" ||
+        item.slug === "xx59-headphones" ||
+        item.slug === "zx9-speaker",
+    );
+  } else if (productName === "xx99-mark-one-headphones") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "xx99-mark-two-headphones" ||
+        item.slug === "xx59-headphones" ||
+        item.slug === "zx9-speaker",
+    );
+  } else if (productName === "xx59-headphones") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "xx99-mark-two-headphones" ||
+        item.slug === "xx99-mark-one-headphones" ||
+        item.slug === "zx9-speaker",
+    );
+  } else if (productName === "zx9-speaker") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "zx7-speaker" ||
+        item.slug === "xx99-mark-one-headphones" ||
+        item.slug === "xx59-headphones",
+    );
+  } else if (productName === "zx7-speaker") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "zx9-speaker" ||
+        item.slug === "xx99-mark-one-headphones" ||
+        item.slug === "xx59-headphones",
+    );
+  } else if (productName === "yx1-earphones") {
+    youMayAlsoLike = base.filter(
+      (item) =>
+        item.slug === "xx99-mark-one-headphones" ||
+        item.slug === "xx59-headphones" ||
+        item.slug === "zx9-speaker",
+    );
+  } else {
+    youMayAlsoLike = base;
+  }
+
   return (
-    <div>
-      <h2 className="text-center text-3xl max-sm:text-2xl font-bold mb-10 max-lg:mb-5">
+    <Transition>
+      <h2 className="text-center text-4xl font-bold mb-10 max-lg:mb-5 max-sm:text-2xl">
         YOU MAY ALSO LIKE
       </h2>
-      <div className="flex justify-between items-center gap-10 px-30 max-lg:px-5 max-lg:gap-5 max-sm:flex-col">
-        <div>
-          <div className="relative">
-            <Image
-              src={XXMARKI}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full max-lg:hidden"
-            />
-            <Image
-              src={XXMARKITablet}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full hidden max-lg:block max-sm:hidden"
-            />
-            <Image
-              src={XXMARKIMobile}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-5 w-full hidden max-sm:block"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-[24px] font-bold mb-10 whitespace-nowrap max-sm:mb-5">
-              XX99 MARK I
-            </h2>
-            <Link
-              href="/"
-              className="text-white font-bold bg-[#D87D4A] px-7 py-3 lg:hover:brightness-130 whitespace-nowrap"
-            >
-              SEE PRODUCT
-            </Link>
-          </div>
-        </div>
-        {/* Test one */}
-        <div>
-          <div className="relative">
-            <Image
-              src={XX59}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full max-lg:hidden"
-            />
-            <Image
-              src={XX59Tablet}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full hidden max-lg:block max-sm:hidden"
-            />
-            <Image
-              src={XX59Mobile}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-5 w-full hidden max-sm:block"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-[24px] font-bold mb-10 whitespace-nowrap max-sm:mb-5">
-              XX59
-            </h2>
-            <Link
-              href="/"
-              className="text-white font-bold bg-[#D87D4A] px-7 py-3 lg:hover:brightness-130 whitespace-nowrap"
-            >
-              SEE PRODUCT
-            </Link>
-          </div>
-        </div>
-        {/* Test two */}
-        <div>
-          <div className="relative">
-            <Image
-              src={ZX9}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full max-lg:hidden"
-            />
-            <Image
-              src={ZX9Tablet}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-10 w-full hidden max-lg:block max-sm:hidden"
-            />
-            <Image
-              src={ZX9Mobile}
-              width={500}
-              height={500}
-              alt="Headphone"
-              className="object-cover rounded-xl mb-5 w-full hidden max-sm:block"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-[24px] font-bold mb-10 whitespace-nowrap max-sm:mb-5">
-              ZX9 SPEAKER
-            </h2>
-            <Link
-              href="/"
-              className="text-white font-bold bg-[#D87D4A] px-7 py-3 lg:hover:brightness-130 whitespace-nowrap"
-            >
-              SEE PRODUCT
-            </Link>
-          </div>
-        </div>
+      <div className="flex justify-between items-center gap-10 px-30 max-lg:px-5 max-lg:gap-5 max-lg:flex-col">
+        {youMayAlsoLike?.map((item) => {
+          return (
+            <div key={item.slug} className="w-full">
+              <Image
+                src={item.image.desktop}
+                width={500}
+                height={500}
+                alt={item.name}
+                className="rounded-xl mb-10 w-full mx-auto max-lg:hidden"
+              />
+              <Image
+                src={item.image.tablet}
+                width={500}
+                height={500}
+                alt={item.name}
+                className="rounded-xl mb-10 w-full hidden max-lg:block max-sm:hidden"
+              />
+              <Image
+                src={item.image.mobile}
+                width={500}
+                height={500}
+                alt={item.name}
+                className="rounded-xl mb-5 w-full hidden max-sm:block"
+              />
+              <div className="flex flex-col items-center">
+                <h2 className="text-4xl font-bold uppercase mb-10 text-nowrap max-sm:mb-5">
+                  {item.name.split(" ").slice(0, -1).join(" ")}
+                </h2>
+                <Link
+                  href={`/${item.category}/${item.slug}`}
+                  className="text-white font-bold bg-[#D87D4A] px-5 py-3 lg:hover:brightness-130 whitespace-nowrap max-sm:text-sm"
+                >
+                  SEE PRODUCT
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </Transition>
   );
 }
-
-export default YouMayAlsoLike;
