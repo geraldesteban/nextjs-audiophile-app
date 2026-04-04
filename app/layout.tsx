134ce241664625739cfe2a1a94e24bb5112e0105
geraldesteban <wakin.esteban@gmail.com>
2026-04-05 01:54:44 +0800

    Fix imports and format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 import type { ReactNode } from "react";
 import { Manrope } from "next/font/google";
 
+import "@/app/_styles/globals.css";
+
+import Header from "@/app/_components/Navigation/Header";
+import Footer from "@/app/footer";
+
 const manrope = Manrope({
   subsets: ["latin"],
 });
-import Header from "@/app/_components/Navigation/Header";
-import "@/app/_styles/globals.css";
-import Footer from "@/app/footer";
 
 export const metadata = {
   title: "Welcome / Audiophile",
 };
 
-type RootLayoutProps = { children: ReactNode };
-
-function RootLayout({ children }: RootLayoutProps) {
+function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${manrope.className} min-h-screen`}>
