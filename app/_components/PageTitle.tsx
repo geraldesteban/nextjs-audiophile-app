4f9ddf4fa6af385902bfa20ca68583494168ad58
geraldesteban <wakin.esteban@gmail.com>
2026-04-05 01:58:08 +0800

    Add Transition

--- app/_components/PageTitle.tsx
+++ app/_components/PageTitle.tsx
@@ -1,18 +1,10 @@
-"use client";
+import { ReactNode } from "react";
 
-import { motion } from "framer-motion";
-
-function Title({ children }: { children: string }) {
+function Title({ children }: { children: ReactNode }) {
   return (
-    <motion.h2
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: false, amount: 0.5 }}
-      transition={{ duration: 0.5 }}
-      className="text-white text-[40px] font-bold text-center max-sm:text-[28px]"
-    >
+    <h2 className="text-white text-[40px] font-bold text-center max-sm:text-[28px]">
       {children}
-    </motion.h2>
+    </h2>
   );
 }
 
