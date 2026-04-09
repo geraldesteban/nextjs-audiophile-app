f4358980ce3d9853312faeb25b56aa73cc5ae1cd
geraldesteban <wakin.esteban@gmail.com>
2026-04-09 14:56:28 +0800

    Delete these files because it was renamed

--- app/account/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from "@/app/_components/Login";
-
-export default function Page() {
-  return (
-    <div className="max-sm:mx-5">
-      <Login />
-    </div>
-  );
-}
--- app/account/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import Signup from "@/app/_components/Signup";
-
-export default function Page() {
-  return (
-    <div className="max-sm:mx-5">
-      <Signup />
-    </div>
-  );
-}
